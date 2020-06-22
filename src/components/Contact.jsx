1e91dd948184b1473560d4e86634721aa974e7e3
unknown <calvinp003@gmail.com>
2020-06-22 15:53:37 -0700

    add branch that adds an currently unworking email form to contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,15 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom'
 
 
 function Contact() {
-    const [emailSubject, setEmailSubject] = useState("")
-
-    const changeEmailSubject = (event) =>{
-        console.log(event);
-        
-    }
-
     return (
       <div className="">
          <h1 className="header">Stay in touch <br/> Follow my social media</h1>
@@ -17,26 +10,29 @@ function Contact() {
             <div className="socialLinks">
                 <a href="mailto:elliederricklewis@gmail.com"><p className="mailLink">✉</p></a>
                 <a href="https://www.facebook.com/elliederricklewis46" target="_blank"><p className="facebookLink">f</p></a>
-                <h1>{emailSubject}</h1>
             </div>
          </div>
         <div className="emailSection">
             <p className="conactInfo">Reach out to me on social media, or send me an email using the form below or by emailing elliederricklewis@gmail.com</p>
-            <form action={`mailto:elliederricklewis@gmail.com?subject=${emailSubject}`} method="get">
+            <form action="send_email.php" method="post">
                 <p className='formItems'>Email:</p>
                 <br/>
                 <input type="text" name="email"/>
                 <br/>
+                <p className='formItems'>Name:</p>
+                <br/>
+                <input type="text" name="name"/>
+                <br/>
                 <p className='formItems'>Subject</p>
                 <br/>
-                <input type="text" name="subject" onChange={console.log('test')
-                }/>
+                <input type="text" name="subject"/>
                 <br/>
                 <p className='formItems'>Message:</p>
                 <br/>
                 <textarea name="body" rows="4" cols="40"/>
                 <br/>
-                <button type="submit">Send</button>
+                <button type='submit' value="send">Send</button>
+                <input type="hidden" name="submit" value='1'/>
                 
             </form>
         </div>
