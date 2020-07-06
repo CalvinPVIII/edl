c68cc08b3bc16e778098bd7138f8f96332543f63
unknown <calvinp003@gmail.com>
2020-07-06 13:37:17 -0700

    Updates

---
+++ precache-manifest.2f1d8eccfc6fadd3627c96b9cadfafed.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5710062830f8befdf008745c427e4b26",
+    "url": "/edl/index.html"
+  },
+  {
+    "revision": "c1411436b09cefad2713",
+    "url": "/edl/static/css/main.8eeaf917.chunk.css"
+  },
+  {
+    "revision": "9a7764fdb5d2b190f5da",
+    "url": "/edl/static/js/2.b225d000.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/edl/static/js/2.b225d000.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1411436b09cefad2713",
+    "url": "/edl/static/js/main.7709fcb3.chunk.js"
+  },
+  {
+    "revision": "04847ccdce706e29fc62",
+    "url": "/edl/static/js/runtime-main.9de0b338.js"
+  },
+  {
+    "revision": "7d917aee6d955eb6af0c50d4adeace35",
+    "url": "/edl/static/media/1.7d917aee.jpg"
+  },
+  {
+    "revision": "9abc0eb2870cf5e8d6260b56898f7357",
+    "url": "/edl/static/media/1.9abc0eb2.jpg"
+  },
+  {
+    "revision": "0f4769f31a64e86d25cff8beb9050924",
+    "url": "/edl/static/media/2.0f4769f3.jpg"
+  },
+  {
+    "revision": "15acba57b594076b969b880242cdb033",
+    "url": "/edl/static/media/2.15acba57.jpg"
+  },
+  {
+    "revision": "527d257f98fb8350fc838cbdbe202ca3",
+    "url": "/edl/static/media/3.527d257f.jpg"
+  },
+  {
+    "revision": "55f63a3b36acc719d6665bbc74b8da86",
+    "url": "/edl/static/media/3.55f63a3b.jpg"
+  },
+  {
+    "revision": "c83179f26b6ab9323c2635c952206c91",
+    "url": "/edl/static/media/4.c83179f2.jpg"
+  },
+  {
+    "revision": "cc1bbc677e93c2a7218123d742924550",
+    "url": "/edl/static/media/5.cc1bbc67.jpg"
+  },
+  {
+    "revision": "01eeaec5a67cda4730f0463638183d65",
+    "url": "/edl/static/media/aboutImg.01eeaec5.jpg"
+  },
+  {
+    "revision": "11178dfb47df4abbc3f94c6c89b1e2f1",
+    "url": "/edl/static/media/profile.11178dfb.jpg"
+  },
+  {
+    "revision": "996c6c1ef66c746e9338655d7d01a9b0",
+    "url": "/edl/static/media/reddingfront.996c6c1e.jpg"
+  },
+  {
+    "revision": "551fa8530e6e79b2abeae0bd2978ab94",
+    "url": "/edl/static/media/sandandsagefront.551fa853.PNG"
+  }
+]);
